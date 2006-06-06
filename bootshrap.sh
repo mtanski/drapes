5ff64e1092161a17d5da4a525217d9ab60d9a11b
Milosz Tanski <mtanski@gmail.com>
2006-06-06 17:00:00 -0400

    [project @ mtanski@gmail.com-20060606210000-889e34ec2025ddc2] Some autotools cleanup/fixes... using gnome-autogen.sh and gconf schema install

--- bootshrap.sh
+++ bootshrap.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 
-aclocal
-automake --gnu --add-missing
-autoconf
-./configure $@
+# fuck it, just use the gnome autogen
+REQUIRED_AUTOMAKE_VERSION="1.9" REQUIRED_AUTOCONF_VERSION="2.53" USE_COMMON_DOC_BUILD="yes" gnome-autogen.sh $@
