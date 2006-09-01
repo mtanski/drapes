31da0876b4c1aea721e5af6e56e7784c6fefe971
Milosz Tanski <mtanski@gmail.com>
2006-09-01 17:41:35 -0400

    [project @ mtanski@gmail.com-20060901214135-926ba7caf5ba426f] * Added documentation from Derek Vandivere, after a long battle with   autotools and docbook.

--- bootshrap.sh
+++ bootshrap.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # fuck it, just use the gnome autogen
-REQUIRED_AUTOMAKE_VERSION="1.9" REQUIRED_AUTOCONF_VERSION="2.53" USE_COMMON_DOC_BUILD="yes" gnome-autogen.sh $@
+REQUIRED_AUTOMAKE_VERSION="1.9" REQUIRED_AUTOCONF_VERSION="2.53" USE_COMMON_DOC_BUILD="yes" USE_GNOME2_MACROS="1" gnome-autogen.sh $@
