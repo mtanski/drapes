9f617a819db98922f06289adc579fefe43418fae
Milosz Tanski <dem@mlap.(none)>
2007-03-20 13:44:28 -0400

    Make sure we create a m4 directory, otherwise bootstrap fails.

--- bootshrap.sh
+++ bootshrap.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
+mkdir m4
+
 # fuck it, just use the gnome autogen
 REQUIRED_AUTOMAKE_VERSION="1.9" REQUIRED_AUTOCONF_VERSION="2.53" USE_COMMON_DOC_BUILD="yes" USE_GNOME2_MACROS="1" gnome-autogen.sh $@
